be9ce191b612417e81547d25fe6f19a98274948e
Sfernandezp <samueljose2209@gmail.com>
2024-06-20 21:19:55 -0400

    nada especial

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+console.log("hola mundo");
+interface persona{
+    nombre:string,
+    edad:number
+}
+let persona1:persona = {
+    nombre:"pedro",
+    edad:20
+}
+console.log(persona1);
