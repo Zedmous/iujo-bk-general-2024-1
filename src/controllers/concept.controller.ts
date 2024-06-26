541e277e5c820aa3e8a3ec415db770bf4b213e0a
Sfernandezp <samueljose2209@gmail.com>
2024-06-26 17:56:03 -0400

    concepcontroller ya funciona solo falta acomodar service

--- src/controllers/concept.controller.ts
+++ src/controllers/concept.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
-import { concept } from "../services/concept.service";
+import { concept, ConceptService } from "../services/concept.service";
 export class ConceptController {
-  constructor() {}
+  private conceptService: ConceptService;
+  constructor() {
+    this.conceptService = new ConceptService();
+  }
   getconcept = async (req: Request, res: Response) => {
     const { status, message, data } = await concept();
     return res.status(status).json({
@@ -17,5 +20,49 @@ export class ConceptController {
       body:req.body
     });
   };
-}
+  getConcepts = (req: Request, res: Response) => {
+    const concepts = this.conceptService.getConcepts();
+    return res.status(200).json(concepts);
+  };
 
+  getConceptById = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const concept = this.conceptService.getConceptById(Number(id));
+    if (concept) {
+      return res.status(200).json(concept);
+    } else {
+      return res.status(404).json({ message: "Concepto no encontrado" });
+    }
+  };
+
+  addConcept = (req: Request, res: Response) => {
+    const { idconcepto, nombrededucciones, valorasignaciones, tipoconcepto } = req.body;
+    const newConcept = {
+      idconcepto,
+      nombrededucciones,
+      valorasignaciones,
+      tipoconcepto
+    };
+    this.conceptService.addConcept(newConcept);
+    return res.status(201).json({ message: "Concepto agregado exitosamente" });
+  };
+
+  deleteConcept = (req: Request, res: Response) => {
+    const { id } = req.params;
+    this.conceptService.deleteConcept(Number(id));
+    return res.status(200).json({ message: "Concepto eliminado exitosamente" });
+  };
+
+  editConcept = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { idconcepto, nombrededucciones, valorasignaciones, tipoconcepto } = req.body;
+    const updatedConcept = {
+      idconcepto,
+      nombrededucciones,
+      valorasignaciones,
+      tipoconcepto
+    };
+    this.conceptService.editConcept(Number(id), updatedConcept);
+    return res.status(200).json({ message: "Concepto editado exitosamente" });
+  };
+}
--- src/services/concept.service.ts
+++ src/services/concept.service.ts
@@ -40,3 +40,34 @@ export const conceptPost = async () => {
         }
     }
 }
+export class ConceptService {
+    private concepts: concept[] = [];
+  
+    constructor() {}
+  
+    getConcepts(): concept[] {
+      return this.concepts;
+    }
+  
+    getConceptById(id: number): concept | undefined {
+      return this.concepts.find((concept) => concept.id === id);
+    }
+  
+    addConcept(concept: concept): void {
+      this.concepts.push(concept);
+    }
+  
+    deleteConcept(id: number): void {
+      const index = this.concepts.findIndex((concept) => concept.id === id);
+      if (index !== -1) {
+        this.concepts.splice(index, 1);
+      }
+    }
+  
+    editConcept(id: number, updatedConcept: Concept): void {
+      const index = this.concepts.findIndex((concept) => concept.id === id);
+      if (index !== -1) {
+        this.concepts[index] = updatedConcept;
+      }
+    }
+  }
