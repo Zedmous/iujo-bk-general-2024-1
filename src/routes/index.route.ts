279cc51e6a6bde806a75df1433c9387480aa3730
Zedmous <59229407+Zedmous@users.noreply.github.com>
2024-06-28 20:54:10 -0400

    Revert "Rrhh/concepts"

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,7 @@
 import testRoute from "./test.route";
 import roleRoute from "./role.route";
-import conceptsRoute from "./concept.route";
 
 export {
     testRoute,
-    roleRoute,
-    conceptsRoute,
+    roleRoute
 }
