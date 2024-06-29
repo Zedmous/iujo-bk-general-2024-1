b6adc22c18ad302333a1ee7a4bd691d152ca6f06
Adrian Aranguren <adrianarangurenjr@gmail.com>
2024-06-29 13:49:07 -0400

    Se agregan las validaciones y las rutas

---
+++ src/routes/table.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { validateFields } from "../middlewares";
+import { TableController } from "../controllers";
+import { TableValidator } from "../validators";
+const tableValidator = new TableValidator();
+const router = Router();
+const tableController=new TableController();
+router.get("/", tableController.all);//http://localhost:3800/api/roles
+router.get("/:id", tableController.one);//http://localhost:3800/api/roles/1
+router.post("/",tableValidator.validateTable,validateFields, tableController.createTable);//http://localhost:3800/api/roles
+router.put("/:id",tableValidator.validateTable,validateFields, tableController.updateTable);//http://localhost:3800/api/roles/1
+router.delete("/:id", tableController.deleteTable);//http://localhost:3800/api/roles/1
+export default router;
