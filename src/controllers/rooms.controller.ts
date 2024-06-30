9629143a6b84a7f375f221f8f34405ea564dc904
richard200327 <150199369+richard200327@users.noreply.github.com>
2024-06-29 22:06:25 -0400

    A;adido endpoint de las habitaciones

---
+++ src/controllers/rooms.controller.ts
@@ -0,0 +1,25 @@
+import { request, Request, response, Response } from "express";
+
+export class RoomsController {
+    constructor(){}
+
+    all = async (req: Request, res: Response) => {
+        return true;
+    }
+
+    one = async (req: Request, res: Response) => {
+        return true;
+    }
+
+    createRooms = async (req: Request, res: Response) => {
+        return true;
+    }
+
+    updateRooms = async (req: Request, res: Response) => {
+        return true;
+    }
+
+    deleteRooms = async (req: Request, res: Response) => {
+        return true;
+    }
+}
