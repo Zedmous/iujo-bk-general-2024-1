5f6fc1d88ba287b9c30aa532a2e0a772245a388c
Adrian Aranguren <adrianarangurenjr@gmail.com>
2024-07-01 07:23:21 -0400

    Detalles

--- src/routes/table.route.ts
+++ src/routes/table.route.ts
@@ -6,8 +6,8 @@ const tableValidator = new TableValidator();
 const router = Router();
 const tableController=new TableController();
 router.get("/", tableController.all);//http://localhost:3800/api/table
-router.get("/:id", tableController.one);//http://localhost:3800/api/roles/1
-router.post("/",tableValidator.validateTable,validateFields, tableController.createTable);//http://localhost:3800/api/roles
-router.put("/:id",tableValidator.validateTable,validateFields, tableController.updateTable);//http://localhost:3800/api/roles/1
-router.delete("/:id", tableController.deleteTable);//http://localhost:3800/api/roles/1
+router.get("/:id", tableController.one);//http://localhost:3800/api/table/1
+router.post("/",tableValidator.validateTable,validateFields, tableController.createTable);//http://localhost:3800/api/table
+router.put("/:id",tableValidator.validateTable,validateFields, tableController.updateTable);//http://localhost:3800/api/table/1
+router.delete("/:id", tableController.deleteTable);//http://localhost:3800/api/table/1
 export default router;
