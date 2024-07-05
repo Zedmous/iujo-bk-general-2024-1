b3c1f3edbba587978261579f0bfa4668e2e7ef8f
Zedmous <zedmous@gmail.com>
2024-07-05 03:20:31 -0400

    fix conflicto otra vez validators

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,6 @@
 export * from './locations.controller';
 export * from './role.controller';
 export * from './test.controller';
-<<<<<<< HEAD
 export * from './concept.controller';
-=======
 export * from './attractions_status.controller';
 export * from './requirements.controller';
->>>>>>> develop
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,9 +1,6 @@
 export * from './test.validators'
 export * from './locations.validators'
 export * from './role.validators'
-<<<<<<< HEAD
 export * from './concept.validator';
-=======
 export * from './attractions_status.validators'
 export * from './requirements.validators'
->>>>>>> develop
