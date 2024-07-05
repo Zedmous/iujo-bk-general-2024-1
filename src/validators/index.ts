0a36c13986fb0bc9ba34fb56771ec8eefe835e1b
Zedmous <zedmous@gmail.com>
2024-07-05 02:58:28 -0400

    detalle de fix

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,8 +1,5 @@
 export * from './test.validators'
 export * from './locations.validators'
 export * from './role.validators'
-<<<<<<< HEAD
 export * from './attractions_status.validators'
-=======
 export * from './requirements.validators'
->>>>>>> develop
