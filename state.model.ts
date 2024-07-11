4b767e4a66c427237ec393effee06c95b1ac9753
ElImparable <rompecuca2@gmail.com>
2024-07-11 09:18:35 -0400

    agregado los modelos de ciudades, estados y paises

---
+++ state.model.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+
+const StateModel = {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    default: true,
+  },
+  id_pais: {
+    type: DataTypes.INTEGER,
+  },
+};
+
+export { StateModel };
