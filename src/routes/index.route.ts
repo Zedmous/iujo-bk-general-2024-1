1c1569fc768c86cc9190941d3ef0ec075c8f9871
Zedmous <zedmous@gmail.com>
2024-07-11 22:21:25 -0400

    detalles de user, login y registro normal

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,8 @@
 import testRoute from "./test.route";
 import roleRoute from "./role.route";
-
+import userRoute from "./user.route";
 export {
     testRoute,
-    roleRoute
+    roleRoute,
+    userRoute
 }
---
+++ src/services/user.service.ts
@@ -0,0 +1,190 @@
+import { User } from "../config";
+import { UserInterface } from "../interfaces";
+
+export const getAll = async () => {
+  try {
+    //consultas a la base de datos van aca
+    /*const users = await User.findAll({
+        where: {
+          status: true,
+        },
+      });*/
+    const users = await User.findAll();
+
+    if(users.length==0){
+      return {
+        message: `No hay usuarios encontrados`,
+        status: 200,
+        data: {
+          users,
+        },
+      };
+    }
+    return {
+      message: `Usuarios encontrados exitosamente`,
+      status: 200,
+      data: {
+        users,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
+
+
+
+export const getOne = async (id: number) => {
+  try {
+    //consultas a la base de datos van aca
+    const user = await User.findOne({ where: { id } }); // Busca el proyecto con título 'Mi Título'
+    if (user === null) {
+      console.log("No encontrado");
+      return {
+        message: `Usuario no encontrado`,
+        status: 404,
+        data: {
+        },
+      };
+    } else {
+      return {
+        message: `Usuario encontrado`,
+        status: 200,
+        data: {
+          user,
+        },
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
+export const create = async (data: UserInterface) => {
+  try {
+    //consultas a la base de datos van aca
+    const user = await User.create({
+      name:data.name,
+      email:data.email,
+      password:data.password,
+      role_id:data.role_id,
+      status:true,
+    });
+
+    return {
+      message: `Creacion del usuario exitoso`,
+      status: 200,
+      data: {
+        user,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
+
+export const update = async (id: number, data: UserInterface) => {
+  try {
+    //consultas a la base de datos van aca
+    const user = await User.update(
+      {
+        name:data.name,
+        email:data.email,
+        role_id:data.role_id,
+        status:true
+      },
+      {
+        where: {
+          id,
+        },
+        returning: true,
+      }
+    );
+
+    return {
+      message: `Actualización del Rol exitoso`,
+      status: 200,
+      data: {
+        user,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
+export const deleted = async (id: number, data: UserInterface) => {
+  try {
+    //consultas a la base de datos van aca
+    const user = await User.update(
+      {
+        status: false,
+        deletedAt: new Date(),
+      },
+      {
+        where: {
+          id,
+        },
+        returning: true,
+      }
+    );
+
+    return {
+      message: `Eliminación del Rol exitoso`,
+      status: 200,
+      data: {
+        user,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
+
+export const getByEmail = async (data: UserInterface) => {
+  try {
+    //consultas a la base de datos van aca
+    const user:UserInterface|any = await User.findOne({ where: { email:data.email } })
+    if (!user) {
+      return {
+        message: `Usuario no encontrado`,
+        status: 404,
+        data: {
+          user
+        },
+      };
+    } else {
+      return {
+        message: `Usuario encontrado`,
+        status: 200,
+        data: {
+          user,
+        },
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
