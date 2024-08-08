ebf53300e0a4e5d37df6c3df2c292d7fba8bc80f
Torpass <pastor.jimenez.0806@gmail.com>
2024-08-07 20:38:17 -0400

    update order validator
    deleted "order_id" validation because it's not necessary in order to create an order, remember "order_id" it's an autoincrement field

--- src/validators/order.validators.ts
+++ src/validators/order.validators.ts
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 class OrderValidator {
   public validateOrder = [
-    body("id").notEmpty().withMessage("Order Id is required"),
+    // body("id").notEmpty().withMessage("Order Id is required"),
     body("supplier_id").notEmpty().withMessage("Supplier id is required"),
     body("supplier_id").isNumeric().withMessage("Supplier id must be numeric"),
   ];
